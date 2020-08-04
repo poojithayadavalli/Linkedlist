b2e8a0efbdd31d264a598765b510e00bd6d61ef5
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-04 09:59:12 +0530

    Create sumofalternatenodes.py

---
+++ sumofalternatenodes.py
@@ -0,0 +1,14 @@
+"""
+
+Given a linked list, the task is to print the sum of the alternate nodes of the linked list.
+
+Examples:
+
+Input : 1 -> 8 -> 3 -> 10 -> 17 -> 22 -> 29 -> 42
+Output : 50
+Alternate nodes : 1 -> 3 -> 17 -> 29
+
+Input : 10 -> 17 -> 33 -> 38 -> 73
+Output : 116
+Alternate nodes : 10 -> 33 -> 73
+"""
