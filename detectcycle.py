37c15a6effb4fa88f560d80b4ed930bc4894e8d0
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-04 09:49:02 +0530

    Create detectcycle.py

---
+++ detectcycle.py
@@ -0,0 +1,7 @@
+"""
+
+Given a linked list with n nodes, check if the linked list has loop or not.
+
+loop in a linked list is defined as a node connecting with another node from which it has a path.
+
+"""
