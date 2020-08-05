4b999404c60a635a1c859fd55c4496f77f8f4dfb
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-05 09:59:26 +0530

    Create replaceduplicatenodes.py

---
+++ replaceduplicatenodes.py
@@ -0,0 +1,16 @@
+"""
+Given a linked list that contains some random integers from 1 to n with many duplicates. 
+
+Replace each duplicate element that is present in the linked list with the values n+1, n+2, n+3 and so on(starting from left to right in the given linked list).
+
+Examples:
+
+Input : 1 3 1 4 4 2 1
+Output : 1 3 5 4 6 2 7
+Replace 2nd occurrence of 1 with 5 i.e. (4+1)
+        2nd occurrence of 4 with 6 i.e. (4+2)
+        3rd occurrence of 1 with 7 i.e. (4+3)
+
+Input : 1 1 1 4 3 2 2
+Output : 1 5 6 4 3 2 7
+"""
