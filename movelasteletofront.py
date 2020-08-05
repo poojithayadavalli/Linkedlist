32649444573bd00afd7b09a138f5d7a19143bce0
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-05 09:53:22 +0530

    Create movelasteletofront.py

---
+++ movelasteletofront.py
@@ -0,0 +1,6 @@
+"""
+Write a function that moves the last element to the front in a given Singly Linked List. 
+
+For example, if the given Linked List is 1->2->3->4->5, then the function should change the list to 5->1->2->3->4
+
+"""
