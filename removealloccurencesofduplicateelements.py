b587ebbc2309ac2216b7b1c7a0aea97cf2467ced
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-05 10:01:19 +0530

    Create removealloccurencesofduplicateelements.py

---
+++ removealloccurencesofduplicateelements.py
@@ -0,0 +1,12 @@
+"""
+Given a sorted linked list, delete all nodes that have duplicate numbers (all occurrences), leaving only numbers that appear once in the original list.
+
+Examples:
+
+Input : 23->28->28->35->49->49->53->53
+Output : 23->35
+
+Input : 11->11->11->11->75->75
+Output : empty List
+
+"""
