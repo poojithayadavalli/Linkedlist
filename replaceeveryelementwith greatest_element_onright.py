1dc66b0cf11d0852dc58ace05c205c37cb75d62d
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-05 09:56:49 +0530

    Create replaceeveryelementwith greatest_element_onright.py

---
+++ replaceeveryelementwith greatest_element_onright.py
@@ -0,0 +1,13 @@
+"""
+Given a linked list, the task is to find the Next Greater Element for every node of the linked list.
+Note: For nodes with no next greater element, store -1 in the result.
+
+Examples:
+
+Input: linked list = [2, 1, 5]
+Output: [5, 5, -1]
+
+Input: linked list = [2, 7, 4, 3, 5]
+Output: [7, -1, 5, 5, -1]
+
+"""
